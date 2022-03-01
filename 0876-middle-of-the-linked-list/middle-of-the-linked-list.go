e174a04c082b8cb188457d1636c8895d8b3153ea
TT <taozhang.tt@gmail.com>
2022-03-01 16:52:22 +0800

    876. 链表的中间结点

---
+++ 0876-middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -0,0 +1,34 @@
+package middleofthelinkedlist
+
+// 876. 链表的中间结点
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 返回链表中间节点，有两个中间节点时，返回后一个
+func middleNode(head *ListNode) *ListNode {
+	prev := &ListNode{Next: head}
+	slow, fast := prev, prev
+	for fast != nil {
+		if fast.Next == nil {
+			return slow.Next
+		}
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	return slow
+}
+
+// 返回链表中间节点，有两个中间节点时，返回前一个
+func middleNode2(head *ListNode) *ListNode {
+	prev := &ListNode{Next: head}
+	slow, fast := prev, prev
+	for fast != nil {
+		if fast.Next == nil {
+			return slow
+		}
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	return slow
+}
---
+++ 0876-middle-of-the-linked-list/middle-of-the-linked-list_test.go
@@ -0,0 +1,45 @@
+package middleofthelinkedlist
+
+import (
+	"testing"
+
+	. "github.com/taozhang-tt/spadger/pkg/testing/xconvey"
+)
+
+func Test(t *testing.T) {
+	Convey("Test middleNode", t, func() {
+		head := &ListNode{1, nil}
+		middle := middleNode(head)
+		So(middle.Val, ShouldEqual, 1)
+
+		head = &ListNode{1, &ListNode{2, nil}}
+		middle = middleNode(head)
+		So(middle.Val, ShouldEqual, 2)
+
+		head = &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+		middle = middleNode(head)
+		So(middle.Val, ShouldEqual, 2)
+
+		head = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
+		middle = middleNode(head)
+		So(middle.Val, ShouldEqual, 3)
+	})
+
+	Convey("Test middleNode2", t, func() {
+		head := &ListNode{1, nil}
+		middle := middleNode2(head)
+		So(middle.Val, ShouldEqual, 1)
+
+		head = &ListNode{1, &ListNode{2, nil}}
+		middle = middleNode2(head)
+		So(middle.Val, ShouldEqual, 1)
+
+		head = &ListNode{1, &ListNode{2, &ListNode{3, nil}}}
+		middle = middleNode2(head)
+		So(middle.Val, ShouldEqual, 2)
+
+		head = &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}}
+		middle = middleNode2(head)
+		So(middle.Val, ShouldEqual, 2)
+	})
+}
