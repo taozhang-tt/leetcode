0b03ab8791aadc5e566d69b616e3f12d47e21803
TT <taozhang.tt@gmail.com>
2022-07-19 10:11:03 +0800

    25. K 个一组翻转链表

--- 0025-reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
+++ 0025-reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
@@ -1,12 +1,14 @@
 package reversenodesinkgroup
 
 // 25. K 个一组翻转链表
+// https://leetcode.cn/problems/reverse-nodes-in-k-group/submissions/
 
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+// 递归做法
 func reverseKGroup(head *ListNode, k int) *ListNode {
 	prev := &ListNode{Next: head}
 	for i := 0; i < k; i++ {
@@ -22,33 +24,28 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 	return prev
 }
 
-// 翻转链表
+// 翻转链表、递归做法
 func reverse(head *ListNode) *ListNode {
-	var prev *ListNode
-	for head != nil {
-		head.Next, head, prev = prev, head.Next, head
+	if head == nil || head.Next == nil {
+		return head
 	}
-	return prev.Next
+	root := reverse(head.Next)
+	head.Next.Next, head.Next = head, nil
+
+	return root
 }
 
-// k 个一组切割链表
-func cutK(head *ListNode, k int) []*ListNode {
-	ret := make([]*ListNode, 0)
-	if head == nil {
-		return ret
+//////////////////////////////
+
+// 直接翻转
+func reverseKGroup2(head *ListNode, k int) *ListNode {
+	dumy := &ListNode{Next: head}
+	prev := dumy
+	for prev != nil {
+		head, tail := reverseK2(prev.Next, k)
+		prev.Next, prev = head, tail
 	}
-	prev := &ListNode{Next: head}
-	for i := 0; i < k; i++ {
-		prev = prev.Next
-		if prev == nil {
-			ret = append(ret, head)
-			return ret
-		}
-	}
-	ret = append(ret, head)
-	ret = append(ret, cutK(prev.Next, k)...)
-	prev.Next = nil
-	return ret
+	return dumy.Next
 }
 
 // 翻转链表的前k个节点，k <= 节点数
@@ -61,4 +58,22 @@ func reverseK(head *ListNode, k int) *ListNode {
 	}
 	root.Next = head
 	return prev
+}
+
+// 翻转链表的前k个节点，不足k个不翻转，返回翻转部分的头尾节点
+func reverseK2(head *ListNode, k int) (*ListNode, *ListNode) {
+	p, cnt := head, 0
+	for p != nil && cnt != k {
+		cnt, p = cnt+1, p.Next
+	}
+	if cnt < k {
+		return head, nil
+	}
+	var prev *ListNode
+	root := head
+	for k > 0 {
+		head.Next, head, prev, k = prev, head.Next, head, k-1
+	}
+	root.Next = head
+	return prev, root
 }
